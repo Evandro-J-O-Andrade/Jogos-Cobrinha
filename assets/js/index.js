e3989286a65736378d8532b8fee55db862662020
Evandro Andrade <evandro_j.o.a@hotmail.com>
2024-09-19 06:25:31 -0300

    Jogo da cobrinha

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+
+window.onload = function () {
+
+    var stage = document.getElementById('stage');
+    var ctx = stage.getContext("2d");
+
+    setInterval(game, 60);
+
+    const vel = 1;
+    var vx = 0, vy = 0;
+    var px = 10, py = 15;
+    var lp = 20;
+    var qp = 20;
+    var ax = 15, ay = 15;
+    var trail = [];
+    tail = 5;
+
+    function game() {
+        px += vx;
+        py += VY;
+
+        if (px < 0) {
+            px = qp - 1;
+        }
+
+        if (px > qp - 1) {
+            px = 0;
+        }
+
+        if (py < 0) {
+            py = qp - 1;
+        }
+
+        if (py > qp - 1) {
+            py = 0;
+        }
+        ctx.fillStyle = "black";
+        ctx.fillRect (0, 0, stage.Width, stage.height);
+
+
+    }
+}
+
