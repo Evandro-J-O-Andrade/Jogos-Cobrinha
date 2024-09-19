963af16b8373aba6d2483a50ba3182cd52af48f5
Evandro Andrade <evandro_j.o.a@hotmail.com>
2024-09-19 06:35:13 -0300

    Up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,5 @@
-
 window.onload = function () {
-
-    var stage = document.getElementById('stage');
+    var stage = document.getElementById("stage");
     var ctx = stage.getContext("2d");
 
     setInterval(game, 60);
@@ -13,31 +11,26 @@ window.onload = function () {
     var qp = 20;
     var ax = 15, ay = 15;
     var trail = [];
-    tail = 5;
+    var tail = 5;
 
     function game() {
         px += vx;
-        py += VY;
+        py += vy;
 
         if (px < 0) {
             px = qp - 1;
         }
-
         if (px > qp - 1) {
             px = 0;
         }
-
         if (py < 0) {
             py = qp - 1;
         }
-
         if (py > qp - 1) {
             py = 0;
         }
+
         ctx.fillStyle = "black";
-        ctx.fillRect (0, 0, stage.Width, stage.height);
-
-
+        ctx.fillRect(0, 0, stage.width, stage.height);
     }
 }
-
